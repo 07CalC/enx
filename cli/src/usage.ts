979b87a1d678ae7737bcab2e30186de1a1463612
07calc <maheshwarivinayak90@gmail.com>
2026-07-07 14:19:55 +0530

    better usage print

--- cli/src/usage.ts
+++ cli/src/usage.ts
@@ -1,93 +1,56 @@
-export function authUsage() {
-  console.log(`
-Authentication:
-  enx signup
-      Sign up with name, email and password.
-
-  enx login
-      Log in and save the authentication token locally.
-`);
-}
-
 export function projectUsage() {
   console.log(`
-Projects:
-  enx project create
-      Create a new project.
-
-  enx project list
-      List all projects.
-
-  enx project update
-      Update a project's name.
-
-  enx project delete
-      Delete a project.
+Usage:
+  enx project create                      Create a new project
+  enx project list                        List all projects
+  enx project update                      Update a project's name
+  enx project delete                      Delete a project
 `);
 }
 
 export function envUsage() {
   console.log(`
-Environments:
-  enx <project> env create
-      Create a new environment.
-
-  enx <project> env list
-      List all environments in a project.
-
-  enx <project> <environment> update
-      Update an environment's name.
-
-  enx <project> <environment> delete
-      Delete an environment.
+Usage:
+  enx <project> env create                Create an environment
+  enx <project> env list                  List environments
+  enx <project> <env> update              Update an environment's name
+  enx <project> <env> delete              Delete an environment
 `);
 }
 
 export function variableUsage() {
   console.log(`
-Variables:
-  enx <project> <environment> var create
-      Create a new variable.
-
-  enx <project> <environment> var create --file <path>
-      Create variables from a file.
-
-  enx <project> <environment> var list
-      List all variables.
-
-  enx <project> <environment> var inject
-      Inject variables into the current shell session.
-
-  enx <project> <environment> var fetch
-      Print variables in .env format.
-
-  enx <project> <environment> var fetch --file <path>
-      Write variables to a file.
+Usage:
+  enx <project> <env> var create          Create a variable
+  enx <project> <env> var create --file   Create variables from a .env file
+  enx <project> <env> var list            List all variables
+  enx <project> <env> var inject          Print export statements for eval
+  enx <project> <env> var fetch           Print variables in .env format
+  enx <project> <env> var fetch --file    Write variables to a file
 `);
 }
 
 export function apiKeyUsage() {
   console.log(`
-API Keys:
-  enx api-key create
-      Create a new API key.
+Usage:
+  enx api-key create                      Create an API key
+  enx api-key list                        List API keys
+  enx api-key delete                      Delete an API key
+`);
+}
 
-  enx api-key list
-      List all API keys.
-
-  enx api-key delete
-      Delete an API key.
+export function authUsage() {
+  console.log(`
+Usage:
+  enx login                               Log in with email and password
+  enx signup                              Sign up with name, email and password
 `);
 }
 
 export function configUsage() {
   console.log(`
-Configuration:
-  enx config set url <url>
-      Set the API URL.
-
-Defaults:
-  API URL: http://localhost:8787
+Usage:
+  enx config set url <url>                Set the API URL
 `);
 }
 
@@ -97,16 +60,14 @@ Enx - Environment Variables Manager
 
 Usage:
   enx <command> [options]
-`);
 
-  authUsage();
-  projectUsage();
-  envUsage();
-  variableUsage();
-  apiKeyUsage();
-  configUsage();
+Commands:
+  login, signup           Authentication
+  project                 Manage projects
+  api-key                 Manage API keys
+  config                  Configuration
 
-  console.log(`
-Run "enx <command> --help" for more information on a command.
+  <project> env           Manage environments
+  <project> <env> var     Manage variables
 `);
 }
